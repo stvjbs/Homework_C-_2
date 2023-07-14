102ba7f91311a5c17b4e6d9bf1bff1831b15bbe2
Yasha <yarashukartem@icloud.com>
2023-07-14 11:35:13 +0300

    HW1+2_ready

---
+++ Homework #1/Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Homework. Lesson 1.
+// Task 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine("Input the first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input the second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if(num1 > num2)
+//     Console.Write($"Большее число - {num1}, а меньшее - {num2}");
+//     else
+//     Console.Write($"Большее число - {num2}, а меньшее - {num1}");
+
+// Task 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine("Input the first number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input the second number");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input the third number");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// int max = num1;
+// if(max < num2)
+//     max = num2;
+// if(max < num3)
+//     max = num3;
+// Console.WriteLine("Максимальное число - " + max);
+
+// Task 3: Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+
+// Console.WriteLine("Input the number");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// if(num1 % 2 == 0)
+//     Console.WriteLine("Yes");
+//     else
+//     Console.WriteLine("No");
+
+// Task 4: Напишите программу, которая на вход принимает число (N),
+//  а на выходе показывает все чётные числа от 1 до N.
+
+// Console.WriteLine("Input the number.");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if(num>1)
+// {
+//     Console.Write("Все четные числа от 1 до " + num + " - ");
+//     while(num>4)
+// {
+//     if(num % 2 == 0)
+//     {
+//     num = num - 2;
+//     Console.Write(num + ", ");
+//     }
+//     else
+//     {
+//     num = num - 1;
+//     Console.Write(num + ", ");
+//     }
+// }
+//     Console.Write("2.");
+// }
+// else
+// Console.Write("Четных чисел между 1 и " + num + " нет");
+
