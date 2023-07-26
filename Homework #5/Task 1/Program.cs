75799be70f6661a06f0dc20e89e7e79b817e3ebb
Yasha <yarashukartem@icloud.com>
2023-07-26 11:33:50 +0300

    New_Folder

---
+++ Homework #5/Task 1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+int FindEvenNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+int size = 5;
+int min = 100;
+int max = 999;
+
+int[] myArray = CreateRandomArray(size, min, max);
+ShowArray(myArray);
+Console.WriteLine($"Sum of even numbers in array =  {FindEvenNumber(myArray)}");
---
+++ Homework #5/Task 2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 36: Задайте одномерный массив, 
+// заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+int SumOfOddIndexNumbers(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int size = 8;
+int min = 1;
+int max = 10;
+
+int[] myArray = CreateRandomArray(size, min, max);
+ShowArray(myArray);
+Console.WriteLine($"Sum of odd-index numbers in array =  {SumOfOddIndexNumbers(myArray)}");
